b931602401407cf43f9caba4eae8178ceffce32b
Abdullah-Nisar <89594904+Abdullah-Nisar@users.noreply.github.com>
2022-05-14 13:30:38 +0500

    Add files via upload

--- sketch_may12a.ino
+++ sketch_may12a.ino
@@ -11,30 +11,40 @@
 AsyncWebServer server(80);
 
 // REPLACE WITH YOUR NETWORK CREDENTIALS
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "Error 404";
+const char* password = "Abdullah23";
 
-const char* PARAM_INPUT_1 = "input1";
-const char* PARAM_INPUT_2 = "input2";
-const char* PARAM_INPUT_3 = "input3";
+const char* PARAM_INPUT_1 = "00:00";
+const char* PARAM_INPUT_2 = "00:00";
+
+const int output26 = 26;
+
+int time_hour_2 = 0;
+int time_minute_2 = 0;
+long total_time_in_ms_2=0;
 
 // HTML web page to handle 3 input fields (input1, input2, input3)
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html><head>
   <title>ESP Input Form</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    label:
+    {
+      margin-right: 10px;
+    }
+  </style>
   </head><body>
   <form action="/get">
-    input1: <input type="text" name="input1">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input2: <input type="text" name="input2">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input3: <input type="text" name="input3">
-    <input type="submit" value="Submit">
+  <label>Counter time for Switch 1: </label>
+  <input type="time" id="input1" name="input1"><br>
+  <input type="submit" value="Submit">
+  <p id="counter1"></p>
+  <br><br><label>Counter time for Switch 2: </label>
+  <input type="time" id="input2" name="input2"><br>
+  <input type="submit" value="Submit">
+  <p id="counter2"></p>
+  <br><br>  
   </form>
 </body></html>)rawliteral";
 
@@ -73,24 +83,28 @@ void setup() {
       inputMessage = request->getParam(PARAM_INPUT_2)->value();
       inputParam = PARAM_INPUT_2;
     }
-    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
-    else if (request->hasParam(PARAM_INPUT_3)) {
-      inputMessage = request->getParam(PARAM_INPUT_3)->value();
-      inputParam = PARAM_INPUT_3;
-    }
     else {
       inputMessage = "No message sent";
       inputParam = "none";
     }
     Serial.println(inputMessage);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-                                     + inputParam + ") with value: " + inputMessage +
-                                     "<br><a href=\"/\">Return to Home Page</a>");
+    request->send(200, "text/html", index_html);
   });
   server.onNotFound(notFound);
   server.begin();
+
+    pinMode(output26, OUTPUT);
 }
 
 void loop() {
+//  Serial.print(PARAM_INPUT_1);
+  int time_hour_1 = (String(PARAM_INPUT_1[0]) + String(PARAM_INPUT_1[1])).toInt();
+  int time_minute_1 = (String(PARAM_INPUT_1[3]) + String(PARAM_INPUT_1[4])).toInt();
+  long total_time_in_ms_1 = (time_hour_1 * 3600 + time_minute_1 * 60) * 1000;
+  Serial.println(total_time_in_ms_1);
+  delay(10000);
+  digitalWrite(output26, LOW);
+  delay(total_time_in_ms_1);
+  digitalWrite(output26, HIGH);
   
 }
